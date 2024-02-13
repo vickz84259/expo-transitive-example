b566fed15dd0540b28dc392507e7c44a4d18d22d
Victor Otieno <victor@slick.co.ke>
2024-02-13 15:04:55 +0300

    Add minimal code needed.
    Signed-off-by: Victor Otieno <victor@slick.co.ke>

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Talkjs from '@talkjs/expo';
+
 export default function App() {
+  Talkjs.getConversationBuilder('test');
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
